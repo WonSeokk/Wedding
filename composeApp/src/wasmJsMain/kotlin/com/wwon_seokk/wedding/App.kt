2916b31e4e9762e03a4984fdc6a0d0ae2179a6de
Wilson <wilson@jejememe.com>
2025-04-27 22:49:22 +0900

    chore: wip

--- composeApp/src/wasmJsMain/kotlin/com/wwon_seokk/wedding/App.kt
+++ composeApp/src/wasmJsMain/kotlin/com/wwon_seokk/wedding/App.kt
@@ -4,30 +4,47 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 import org.jetbrains.compose.resources.painterResource
 
 import wedding.composeapp.generated.resources.Res
-import wedding.composeapp.generated.resources.compose_multiplatform
+import wedding.composeapp.generated.resources.test
 
 @Composable
 fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
+        LaunchedEffect(Unit) {
+            delay(500)
+            showContent = true
+        }
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
             AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
                 Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+                    Image(
+                        modifier = Modifier.heightIn(250.dp),
+                        painter = painterResource(Res.drawable.test),
+                        contentDescription = null
+                    )
+                    Text(
+                        text = "떠영♥️원석",
+                        style = fontFamily.h2
+                    )
                 }
             }
         }
